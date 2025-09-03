fc762b13d176f6ed68f5da8a79798c670699c0da
andigans <74663590+andi-arch1@users.noreply.github.com>
2025-09-03 14:52:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from datetime import datetime
-
+import requests 
 st.set_page_config(page_title="C-PRO Multi Page App", layout="wide")
 
 # Sidebar untuk navigasi halaman
